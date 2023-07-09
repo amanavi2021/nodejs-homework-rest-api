2457e535a58ea4089de51a11e820985991da1330
Nadiia Maltseva <amanavi2021@gmail.com>
2023-07-09 23:20:19 +0300

    fix schema for validation

--- schemas/addShema.js
+++ schemas/addShema.js
@@ -2,8 +2,8 @@ const Joi = require("joi");
 
 const addShema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required()
+    email: Joi.string().email({minDomainSegments:2}).required(),
+    phone: Joi.string().length(10).pattern(/^[0-9]+$/).required()
 })
 
 module.exports = addShema;
