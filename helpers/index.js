d9b5e53e51c351c619768f9bebea00dc66126b62
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-10 22:28:44 +0300

    adds email

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError= require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
+const transporter = require("./transporter");
 
 module.exports = {
     HttpError,
     ctrlWrapper,
-    handleMongooseError
+    handleMongooseError,
+    transporter,
 }
--- models/user.js
+++ models/user.js
@@ -24,6 +24,14 @@ const userSchema = new Schema({
       avatarURL: {
         type: String,
         
+      },
+      verify: {
+        type: Boolean,
+        default: false,
+      },
+      verificationToken: {
+        type: String,
+        required: [true, 'Verify token is required'],
       }
 },{versionKey:false, timestamps: true});
 
@@ -42,12 +50,17 @@ const loginSchema = Joi.object({
 
  const updateSubscriptionSchema = Joi.object({
   subscription: Joi.valid(...['starter', 'pro', 'business']).required(),
-})
+});
+
+ const emailSchema = Joi.object({
+  email: Joi.string().email({minDomainSegments:2}).required(),
+ })
 
  const schemas = {
     registrationSchema,
     loginSchema, 
-    updateSubscriptionSchema
+    updateSubscriptionSchema,
+    emailSchema
  };
 
  const User = model("user", userSchema);
